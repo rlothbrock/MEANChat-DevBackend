b0226a397b3d1f4ae19e3bf7abe0786f7f92d717
robb <roberto.manganelly@gmail.com>
2020-10-16 00:46:35 -0400

    fixing bodyFilter

--- utils/route.tools/bodyFilter.js
+++ utils/route.tools/bodyFilter.js
@@ -1,7 +1,9 @@
 exports.bodyFilter =function(request, ...allowedFields){
     const filtered = Object.create({});
     for (let field of allowedFields){
-        filtered[field] = request.body[field]
+        if( !!request.body[field] ){
+        	filtered[field] = request.body[field];
+        }
     };
     return filtered 
 };
